410c93fedb982ac38cb80de2cc46a802452fd15b
Cody Caro <codyccaro@gmail.com>
2017-10-31 00:11:51 -0700

    Created the folder

---
+++ ProjectBoost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }else
+        {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.right);
+        }else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.right);
+        }
+    }
+}
